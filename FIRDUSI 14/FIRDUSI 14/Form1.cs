de9fda8c6a45d1892ace7d06fc55d2056b80a8f8
Firdusi <82368409+Firdusi@users.noreply.github.com>
2021-12-16 10:43:01 +0300

    Add files via upload

---
+++ FIRDUSI 14/FIRDUSI 14/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIRDUSI_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int b = Convert.ToInt32(textBox1.Text);
+            if (radioButton1.Checked)
+            {
+                int oplata = b * 500;
+                textBox2.Text = Convert.ToString(oplata);
+            }
+
+            
+            if (radioButton2.Checked) 
+            {
+                int oplata = b * 100;
+                textBox2.Text = Convert.ToString(oplata);
+            }
+        }
+    }
+}
